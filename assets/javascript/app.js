fda7a7e87c1a5f3ded499366a6a0dc68a61fc0a4
Simon Cain <rudenik@gmail.com>
2018-05-18 11:04:45 -0400

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+
+var config = {
+  apiKey: "AIzaSyAe0-fpKphHdChAkM0WGFn2gH3WZf3_VZ0",
+  authDomain: "rps-multiplayer-4f7fd.firebaseapp.com",
+  databaseURL: "https://rps-multiplayer-4f7fd.firebaseio.com",
+  projectId: "rps-multiplayer-4f7fd",
+  storageBucket: "rps-multiplayer-4f7fd.appspot.com",
+  messagingSenderId: "596398391346"
+};
+firebase.initializeApp(config);
