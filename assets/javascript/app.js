41b299a5081bffe2dfbb390b238f77985db05170
Simon Cain <rudenik@gmail.com>
2018-05-22 21:18:52 -0400

    got the onDisconnect() working properly FINALLY. I think the rest won't be so bad.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,11 +13,29 @@ var ref = FBDB.ref();
 var connections = FBDB.ref("/connections");
 var connectedRef = FBDB.ref(".info/connected");
 var numberOfConnected;
+var playerNumber;
+var connectionID;
+var playerThrow;
+var controlsOn = false;
+
+// var amOnline = connectedRef;
+// var userRef = FBDB.ref("/presence/"+userid);
+
+// amOnline.on('value', function(snap){
+//   if(snap.val()){
+//     userRef.onDisconnect.remove();
+//     userRef.set(true);
+//   }
+// });
+
 
 connectedRef.on("value", function(snap) {
   if (snap.val()) {
+    
+    // connectionID = snap.val();
+    // console.log(connectionID);
     var con = connections.push(true);
-    con.onDisconnect().remove();
+    con.onDisconnect().remove()
   }
 });
 
@@ -33,7 +51,7 @@ $("#playerstartbutton").on("click", function(event) {
   name = $("#playername-input")
     .val()
     .trim();
-  console.log($(".active"));
+  // console.log($(".active"));
   if ($(".active").length < 1) {
     alert("pick an avatar");
   } else {
@@ -45,7 +63,8 @@ $("#playerstartbutton").on("click", function(event) {
       avatar: avatar,
       wins: 0,
       loses: 0,
-      dateAdded: firebase.database.ServerValue.TIMESTAMP
+      dateAdded: firebase.database.ServerValue.TIMESTAMP//,
+      //conID : connectionID
     };
     
     FBDB.ref()
@@ -54,6 +73,7 @@ $("#playerstartbutton").on("click", function(event) {
       .then(function(onceShot) {
         // console.log(onceShot.val());
         if (onceShot.numChildren() == 0) {
+            playerNumber=1;
           console.log("There are no Players");
           ref
             .child("Players")
@@ -63,7 +83,10 @@ $("#playerstartbutton").on("click", function(event) {
             .empty()
             .css("height", "88px");
             drawPlayer(player, $("#player1"));
+            // drawControls($("#player1"));
+            ref.child("Players").child(1).onDisconnect().remove();
         } else if (onceShot.numChildren() == 1) {
+            playerNumber=2;
           console.log("There is one Player");
           ref
             .child("Players")
@@ -73,6 +96,7 @@ $("#playerstartbutton").on("click", function(event) {
             .empty()
             .css("height", "88px");
             drawPlayer(player, $("#player2"));
+            ref.child("Players").child(2).onDisconnect().remove();
         } else if (onceShot.numChildren() == 2) {
           console.log("There are 2 players");
           alert("Too many Players, sorry");
@@ -85,6 +109,7 @@ $("#playerstartbutton").on("click", function(event) {
 connections.on("value", function(snap) {
   console.log(snap.numChildren());
   numberOfConnected = snap.numChildren();
+
 });
 FBDB.ref()
   .child("/Players")
@@ -93,9 +118,12 @@ FBDB.ref()
     console.log(onceShot.val());
     if (onceShot.numChildren() == 0) {
       console.log("There are no Players");
+      // playerNumber=1;
     } else if (onceShot.numChildren() == 1) {
       console.log("you are player 2");
+      playerNumber=2;
       drawPlayer(onceShot.child("1").val(), $("#player1"));
+      
     } else if (onceShot.numChildren() == 2) {
       console.log("There are 2 players");
     //   console.log(onceShot.child("1").val().name);
@@ -145,3 +173,78 @@ function drawPlayer(playerInfo, placeToBeDrawn){
       placeToBeDrawn.append(losesLabel);
 }
 
+function drawControls(where){
+    var controlDiv=$("<div>");
+    controlDiv.addClass("row").attr("id", "throws");
+    var rockDiv = $("<div>");
+    rockDiv.addClass("col").text("ROCK").attr("id", "rock").addClass("throw");
+    var paperDiv = $("<div>");
+    paperDiv.addClass("col").text("PAPER").attr("id", "paper").addClass("throw");
+    var scissorDiv = $("<div>");
+    scissorDiv.addClass("col").text("SCISSOR").attr("id", "scissor").addClass("throw");
+    controlDiv.append(rockDiv);
+    controlDiv.append(paperDiv);
+    controlDiv.append(scissorDiv);
+    where.append(controlDiv);
+}
+$("#gamearea").on("click", ".throw", function(){
+    console.log($(this).attr("id"));
+    $(this).siblings().css("display", "none");
+    playerThrow=$(this).attr("id");
+    FBDB.ref("/Throws").child(playerNumber).set(playerThrow);
+    FBDB.ref("/Throws").child(playerNumber).onDisconnect().remove();
+    controlsOn=false;
+})
+
+ref.child("/Players").on("value", function(snap){
+    console.log("There are " + snap.numChildren() + " players");
+    if ((snap.numChildren() === 2)){// && (!controlsOn)){
+        console.log(controlsOn);
+        console.log("I am Player: " + playerNumber);
+        if(playerNumber==1){
+            drawControls($("#player1"));
+            drawPlayer(snap.child("2").val(), $("#player2"));
+            controlsOn=true;
+        }
+        if (playerNumber==2){//broken
+        // if((playerNumber==2)&&(controlsOn)){
+            drawControls($("#player2"));
+            drawPlayer(snap.child("1").val(), $("#player1"));
+            controlsOn=true;
+        }
+    }
+});
+
+ref.child("/Throws").on("value", function(snap){
+    console.log("There are " + snap.numChildren() + " throws");
+    if(snap.numChildren() === 2){
+      console.log("we have a game here");
+      if((snap.child(1).val()=="paper")&&(snap.child(2).val()=="rock")){
+        console.log("Player1 wins");
+        $("#battlearea").text("Player 1 Wins");
+      }else if((snap.child(1).val()=="rock")&&(snap.child(2).val()=="scissor")){
+        console.log("Player1 wins");
+        $("#battlearea").text("Player 1 Wins");
+      }else if((snap.child(1).val()=="scissor")&&(snap.child(2).val()=="paper")){
+        console.log("Player1 wins");
+        $("#battlearea").text("Player 1 Wins");
+      }else if(snap.child(1).val()==snap.child(2).val()){
+        console.log("Tie Game");
+        $("#battlearea").text("Draw");
+      }else if((snap.child(2).val()=="paper")&&(snap.child(1).val()=="rock")){
+        console.log("Player2 wins");
+        $("#battlearea").text("Player 2 Wins");
+      }else if((snap.child(2).val()=="rock")&&(snap.child(1).val()=="scissor")){
+        console.log("Player2 wins");
+        $("#battlearea").text("Player 2 Wins");
+      }else if((snap.child(2).val()=="scissor")&&(snap.child(1).val()=="paper")){
+        console.log("Player2 wins");
+        $("#battlearea").text("Player 2 Wins");
+    }
+  }
+});
+
+function drawWin(winner){
+
+
+}
